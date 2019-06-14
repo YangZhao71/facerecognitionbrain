a29d79eca0cdf4126a127d312b1bab8b9ef340f0
Yangmengyuan Zhao <zymy701@gmail.com>
2019-06-14 17:31:55 +0800

    6.14

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+
+function Logo() {
+	return(
+		<Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+ <div className="Tilt-inner"> 👽 </div>
+</Tilt>
+	);
+}
+export default Logo;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Navigation() {
+	return(
+		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+			<p className='f3 dim black pa3 pointer link'>Sign Out</p>
+		</nav>
+	);
+}
+export default Navigation;
